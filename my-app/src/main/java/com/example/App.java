eb1d02455ec8e12cd0f07dbb93e4b736aa1daf9c
cloudTechSneha <saisnehacloud23@gmail.com>
2025-09-30 00:21:20 +0530

    Update App.java

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -1,7 +1,12 @@
 package com.example;
-
+ 
 public class App {
     public int add(int a, int b) {
         return a + b;
+    }
+ 
+    // Optional: add new method
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
