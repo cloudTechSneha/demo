d5301f7e8c386314b2dfa0b8da42ce0a7aade253
cloudTechSneha <saisnehacloud23@gmail.com>
2025-09-30 00:39:04 +0530

    Update App.java

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -7,13 +7,21 @@ public final class App {
 
     /**
      * Adds two numbers.
+     *
+     * @param a first number
+     * @param b second number
+     * @return sum of a and b
      */
     public int add(final int a, final int b) {
         return a + b;
     }
 
     /**
-     * Subtracts b from a.
+     * Subtracts second number from first.
+     *
+     * @param a first number
+     * @param b second number
+     * @return result of a - b
      */
     public int subtract(final int a, final int b) {
         return a - b;
